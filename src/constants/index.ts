136fd48bf52334706e4d0c4772a11ad340ab18e1
CodeWithSubaru <francis.cloydm@gmail.com>
2023-09-07 23:41:33 +0800

    Added Other personal details on contact page

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,7 @@
+const TEMPLATE_ID: string = "__ejs-test-mail-service__";
+const SERVICE_ID: string = "service_fb8e1fh";
+const PUBLIC_KEY: string = "A5rsOdG5LzcIHKVv9";
+
+export { TEMPLATE_ID, PUBLIC_KEY, SERVICE_ID };
+
 export default "animate__animated animate__";
